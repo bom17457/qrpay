ffa9f40602c67381c81d2cffb6af5837fc258735
bom17457 <brombom43531@gmail.com>
2018-08-28 18:24:15 +0700

    edit and add navigation

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Text,View,StatusBar} from 'react-native'
 import {Ionicons} from '@expo/vector-icons'
+
+import Option from './src/layout/Option'
+import Feed from './src/layout/Feed'
+import Nearby from './src/layout/Nearby'
 import Scanner from './src/layout/Scanner'
-import Signup from './src/layout/Signup'
-import Signin from './src/layout/Signin'
 
 import {createBottomTabNavigator} from 'react-navigation'
 
@@ -11,23 +13,30 @@ export default class App extends React.Component{
   render(){
     return(
       <View style={{flex:1,paddingTop:StatusBar.currentHeight}}>
-        <Signin />
+        <BottomNavigator />
       </View>
     )
   }
 }
 const BottomNavigator = createBottomTabNavigator(
   {
-    signup:{screen:Signup},
-    
-  },{
-    initialRouteName:'signup',
+    home:{screen:Feed},
+    nearby:{screen:Nearby},
+    scanner:{screen:Scanner},
+    option:{screen:Option}    
+  },{    
     navigationOptions:({navigation}) =>({
       tabBarIcon:({focused,tintColor}) =>{
         const {routeName} = navigation.state;
         let iconName;
-        if(routeName === 'signup'){
-          iconName = `ios-home${focused ?'':'-outline'}`;
+        if(routeName === 'home'){
+          iconName = `ios-globe${focused ?'':'-outline'}`;          
+        }else if(routeName === 'nearby'){
+          iconName = `ios-pin${focused ?'':'-outline'}`;
+        }else if(routeName === 'option'){
+          iconName = `ios-menu${focused ?'':'-outline'}`;
+        }else if(routeName === 'scanner'){
+          iconName = `ios-qr-scanner${focused ?'':'-outline'}`
         }
 
         return <Ionicons name={iconName} size={25} color={tintColor} />
